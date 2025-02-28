c242e31bd89ddf108d51d9b70d803d1a24b56f90
Steve <nice.lizhi@gmail.com>
2025-02-28 10:15:31 +0000

    add rq

--- config/queue.php
+++ config/queue.php
@@ -65,6 +65,25 @@ return [
             'block_for' => null,
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+            'option' => [
+                'queue' => [
+                    'prioritize_delayed' =>  false,
+                    'queue_max_priority' => 10,
+                ]
+            ],
+        ]
+
     ],
 
     /*
