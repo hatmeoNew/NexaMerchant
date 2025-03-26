0216840800b3d56937e27320ab0aa8526b621359
Steve <34465153+xxl4@users.noreply.github.com>
2025-03-26 16:58:27 +0800

    fix cart rules

---
+++ packages/Nicelizhi/OneBuy/src/Console/Commands/Temp/ChangeProductRuleSave.php
@@ -0,0 +1,16 @@
+<?php
+namespace Nicelizhi\OneBuy\Console\Commands\Temp;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class ChangeProductRuleSave extends Command
+{
+    protected $signature = 'onebuy:change-product-rule-save';
+    protected $description = 'Change product rule re save';
+
+    public function handle()
+    {
+        
+    }
+}
