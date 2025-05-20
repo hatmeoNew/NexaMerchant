3ed3608b7370eb9dcee5dd54ac27b0cb2a9d1ef8
rickon <916675194@qq.com>
2025-05-20 18:13:17 +0800

    custom_suk兼容

--- packages/Nicelizhi/Shopify/src/Console/Commands/Order/PostOdoo.php
+++ packages/Nicelizhi/Shopify/src/Console/Commands/Order/PostOdoo.php
@@ -133,9 +133,9 @@ class PostOdoo extends Command
             $additional = $orderItem['additional'];
             $variant_id = $additional['selected_configurable_option'] ?: $orderItem['product_id'];
             if (empty($additional['product_sku'])) {
-                $additional['product_sku'] = $this->product->where('id', $variant_id)->value('sku');
                 $additional['img'] = $this->product_image->where('product_id', $variant_id)->value('path');
             }
+            $additional['product_sku'] = $this->product->where('id', $variant_id)->value('sku');
 
             $line_item['is_shipping'] = $variant_id == env('ONEBUY_RETURN_SHIPPING_INSURANCE_PRODUCT_ID');
 
