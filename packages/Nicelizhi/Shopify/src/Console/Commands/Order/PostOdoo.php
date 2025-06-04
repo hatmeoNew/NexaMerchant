474ab13776a4ff06aa34241e53250a39b3229664
rickon <916675194@qq.com>
2025-06-04 17:25:00 +0800

    fix bug

--- packages/Nicelizhi/Shopify/src/Console/Commands/Order/PostOdoo.php
+++ packages/Nicelizhi/Shopify/src/Console/Commands/Order/PostOdoo.php
@@ -548,4 +548,14 @@ class PostOdoo extends Command
 
         return $state;
     }
+
+    private function get_content($URL)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $URL);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
 }
