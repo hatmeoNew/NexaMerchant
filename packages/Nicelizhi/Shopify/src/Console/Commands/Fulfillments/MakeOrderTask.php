0d76a3acef0c4142d24aad133ece2234c04b2f23
rickon <916675194@qq.com>
2025-06-16 14:09:12 +0800

    改为循环获取

--- packages/Nicelizhi/Shopify/src/Console/Commands/Fulfillments/MakeOrderTask.php
+++ packages/Nicelizhi/Shopify/src/Console/Commands/Fulfillments/MakeOrderTask.php
@@ -2,7 +2,6 @@
 
 namespace Nicelizhi\Shopify\Console\Commands\Fulfillments;
 
-use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Webkul\Sales\Models\Order;
 use Illuminate\Support\Facades\Artisan;
@@ -11,18 +10,31 @@ class MakeOrderTask extends Command
 {
     protected $signature = 'make:order:task';
 
-    protected $description = '获取待发货订单, 写入队列';
+    protected $description = '获取待发货订单, 发起同步数据脚本';
 
     public $diffDay = 1;
 
     public function handle()
     {
         // 获取待发货订单ID
-        $ids = Order::where('status', '=', 'processing')->where('created_at', '<', Carbon::now()->subDays($this->diffDay))->pluck('id');
-        foreach ($ids as $id) {
-            Artisan::call((new CreateOdoo())->getName(), [
-                '--order_id' => $id,
-            ]);
+        $maxId = 0;
+        $i = 0;
+        $limit = 100;
+        while (true) {
+            $ids = Order::where('status', 'processing')->where('id', '>', $maxId)->orderBy('id', 'ASC')->limit($limit)->pluck('id');
+            $i = $i + $limit;
+
+            if (empty($ids)) {
+                break;
+            }
+            foreach ($ids as $id) {
+                dump($id);
+                Artisan::call((new CreateOdoo())->getName(), [
+                    '--order_id' => $id,
+                ]);
+
+                $maxId = $id;
+            }
         }
     }
 }
