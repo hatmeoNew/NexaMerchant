64f6b3bd44fd6a3acd8e5f7aa09d70bdb44e7784
rickon <916675194@qq.com>
2025-06-16 17:30:52 +0800

    新增邮件接口

--- packages/Nicelizhi/Shopify/src/Models/EmailSendRecords.php
+++ packages/Nicelizhi/Shopify/src/Models/EmailSendRecords.php
@@ -2,12 +2,23 @@
 
 namespace Nicelizhi\Shopify\Models;
 
+use Webkul\Sales\Models\Order;
 use Illuminate\Database\Eloquent\Model;
 
 class EmailSendRecords extends Model
 {
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    const METRIC_TYPE_100 = 100;
+    const METRIC_TYPE_200 = 200;
+    const METRIC_TYPE_300 = 300;
+
+    public static $metric_type_map = [
+        self::METRIC_TYPE_100 => 'Place Order',
+        self::METRIC_TYPE_200 => 'Fulfilled Order',
+        self::METRIC_TYPE_300 => 'Cancelled Order',
+    ];
 
     protected $fillable = [
         'order_id',
@@ -19,4 +30,10 @@ class EmailSendRecords extends Model
         'created_at',
         'updated_at',
     ];
+
+    // 关联订单
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
 }
