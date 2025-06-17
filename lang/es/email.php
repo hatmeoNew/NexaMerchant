9c686a75fe18925817bd9c73e2b715447d770726
rickon <916675194@qq.com>
2025-06-17 20:30:02 +0800

    add lang config

---
+++ lang/es/email.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'payment' => 'Payment',
+    'billing_address' => 'Billing address',
+    'shipping_address' => 'Shipping address',
+    'shipped_confirmation' => 'Confirmación de envío de pedido'
+];
