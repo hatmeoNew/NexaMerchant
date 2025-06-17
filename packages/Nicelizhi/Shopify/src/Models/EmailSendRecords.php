d38a0da991988fc2dbde4f65b02ff8088ff3d382
rickon <916675194@qq.com>
2025-06-17 08:54:51 +0800

    fix bug

--- packages/Nicelizhi/Shopify/src/Models/EmailSendRecords.php
+++ packages/Nicelizhi/Shopify/src/Models/EmailSendRecords.php
@@ -15,45 +15,6 @@ class EmailSendRecords extends Model
     const METRIC_TYPE_300 = 300;
 
     public static $metric_type_map = [
-        self::METRIC_TYPE_100 => 'Place Order',
-        self::METRIC_TYPE_200 => 'Fulfilled Order',
-        self::METRIC_TYPE_300 => 'Cancelled Order',
-    ];
-
-    public static $metric_type_map_name = [
-        'Place Order' => '订单确认',
-        'Fulfilled Order' => '发货确认',
-        'Cancelled Order' => '取消订单',
-    ];
-
-    protected $fillable = [
-        'order_id',
-        'email',
-        'metric_name',
-        'sender',
-        'send_status',
-        'failure_reason',
-        'created_at',
-        'updated_at',
-    ];
-
-    <?php
-
-namespace Nicelizhi\Shopify\Models;
-
-use Webkul\Sales\Models\Order;
-use Illuminate\Database\Eloquent\Model;
-
-class EmailSendRecords extends Model
-{
-    protected $primaryKey = 'id';
-    public $timestamps = false;
-
-    const METRIC_TYPE_100 = 100;
-    const METRIC_TYPE_200 = 200;
-    const METRIC_TYPE_300 = 300;
-
-    public static $metric_type_map = [
         self::METRIC_TYPE_100 => 'Placed Order',
         self::METRIC_TYPE_200 => 'Fulfilled Order',
         self::METRIC_TYPE_300 => 'Cancelled Order',
